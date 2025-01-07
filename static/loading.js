d7b21bb8a038396b88bc607a06a9e77e89397808
namgungcold <105970056+namgungcold@users.noreply.github.com>
2025-01-07 20:37:11 +0900

    Add files via upload

---
+++ static/loading.js
@@ -0,0 +1,9 @@
+// // 로딩 화면 제어 (테스트용)
+// window.addEventListener("load", () => {
+//     const loadingScreen = document.getElementById("loading-screen");
+
+//     // 3초 후 로딩 화면 숨기기 (테스트용)
+//     setTimeout(() => {
+//         loadingScreen.style.display = "none";
+//     }, 3000);
+// });
